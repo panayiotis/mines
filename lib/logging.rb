52714a40b7161c8b7097ba309900f47d87262d18
panos <panos.uoa@gmail.com>
2013-03-03 22:37:10 +0200

    make logging to a mixin

--- lib/logging.rb
+++ lib/logging.rb
@@ -2,16 +2,32 @@
 
 require 'logger'
 
-module Mines
-  class << self
-    attr_accessor :log
-  end
-  
-  # Create Logger
-  @log ||= Logger.new 'log/mines.log'
-  @log.datetime_format= "%H:%M:%S"
-  @log.formatter = proc do |severity, datetime, progname, msg|
-  "#{severity} #{datetime.strftime("%H:%M:%S")}: -- #{msg}\n"
-end
-end
+# Use this module by including it in a class
+module Mines::Logging
+  def log
+    if defined?(@log).nil?
+      @log = Logger.new 'log/mines.log'
+      @log.datetime_format= "%H:%M:%S"
+      @log.formatter = proc do |severity, datetime, progname, msg|
+        "#{severity} #{datetime.strftime("%H:%M:%S")}: -- #{msg}\n"
+      end
+    end
+    @log
+  end # end def
+end # end module
 
+##
+## Old version
+
+#module Mines
+#  class << self
+#    attr_accessor :log
+#  end
+#  
+#  # Create Logger
+#  @log ||= Logger.new 'log/mines.log'
+#  @log.datetime_format= "%H:%M:%S"
+#  @log.formatter = proc do |severity, datetime, progname, msg|
+#    "#{severity} #{datetime.strftime("%H:%M:%S")}: -- #{msg}\n"
+#  end
+#end
